96df5781d49c5fa318f2187c83e699fdfdb90ced
pramit koley <pramitkoley10@gmail.com>
2024-02-09 12:14:52 +0530

    new code 7

--- Udemy_Automation_Assignment_7/src/Task.java
+++ Udemy_Automation_Assignment_7/src/Task.java
@@ -1,5 +1,9 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task {
@@ -9,12 +13,19 @@ public class Task {
 		
 		//This code is created by Pramit koley.
 		
+		
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("window.scrollBy(0 ,500)", "");
+		List<WebElement> a = driver.findElements(By.xpath("//table[@name='courses']/tbody/tr"));
+		int n = a.size();
+		System.out.println("number of Rows:"+n);
 		
-		
+		List<WebElement> b = driver.findElements(By.xpath("//table[@name='courses']/tbody/tr/th"));
+		int m = b.size();
+		System.out.println("number of Column:"+m);
 
 	}
 
