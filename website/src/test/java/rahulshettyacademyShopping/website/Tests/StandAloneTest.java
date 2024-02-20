cb511be6f2d53a06a9b6f8a51170a85b47f22378
pramit koley <pramitkoley10@gmail.com>
2024-02-20 10:58:33 +0530

    updates

--- website/src/test/java/rahulshettyacademyShopping/website/Tests/StandAloneTest.java
+++ website/src/test/java/rahulshettyacademyShopping/website/Tests/StandAloneTest.java
@@ -4,6 +4,9 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,11 +20,11 @@ import rahulshettyacademyShopping.website.TestsComponent.BaseTest;
 public class StandAloneTest extends BaseTest {
 
 	@Test(dataProvider = "getData", groups = "full test")
-	public void submitOrder(String username, String password, String productname) throws IOException {
+	public void submitOrder(HashMap<String,String> input) throws IOException {
 		// TODO Auto-generated method stub
 
 		landingpage landingpage = lunchApplication();
-		ProductCatalogue ProductCatalogue = landingpage.logindata(username, password);
+		ProductCatalogue ProductCatalogue = landingpage.logindata(input.get("username"), input.get("password"));
 
 		boolean ans2 = landingpage.getIncorrectInput();
 		if (ans2 == true) {
@@ -31,6 +34,7 @@ public class StandAloneTest extends BaseTest {
 			// main page
 
 			// String productname = ProductCatalogue.searchName();
+			String productname = input.get("productname");
 			ProductCatalogue.getItemstList();
 			ProductCatalogue.addProductToCart(productname);
 			AddToCartPage AddToCartPage = ProductCatalogue.addToCart();
@@ -57,10 +61,10 @@ public class StandAloneTest extends BaseTest {
 	}
 
 	@Test(dependsOnMethods = "submitOrder", dataProvider = "getData", groups = "full test")
-	public void orderHistry(String username, String password, String productname) throws IOException {
+	public void orderHistry(HashMap<String,String> input) throws IOException {
 
 		landingpage landingpage = lunchApplication();
-		ProductCatalogue ProductCatalogue = landingpage.logindata(username, password);
+		ProductCatalogue ProductCatalogue = landingpage.logindata(input.get("username"), input.get("password"));
 
 		boolean ans2 = landingpage.getIncorrectInput();
 		if (ans2 == true) {
@@ -70,18 +74,35 @@ public class StandAloneTest extends BaseTest {
 
 			OrderHistry OrderHistry = new OrderHistry(driver);
 			OrderHistry.orderButton();
-			Boolean ans = OrderHistry.orderProductList(productname);
+			Boolean ans = OrderHistry.orderProductList(input.get("productname"));
 			Assert.assertTrue(ans);
 			driver.close();
 		}
 
 	}
 
+//	@DataProvider
+//	public Object[][] getData() {
+//		return new Object[][] { { "Pramitkoley@mail.com", "Password@123", "ADIDAS ORIGINAL" },
+//				{ "Password@123", "Pramitkoley@mail.com", "IPHONE 13 PRO" } };
+//
+//	}
+	
 	@DataProvider
 	public Object[][] getData() {
-		return new Object[][] { { "Pramitkoley@mail.com", "Password@123", "ADIDAS ORIGINAL" },
-				{ "Password@123", "Pramitkoley@mail.com", "IPHONE 13 PRO" } };
+		 HashMap<String,String> map = new HashMap<String,String>();
+			map.put("username", "Pramitkoley@mail.com");
+			map.put("password", "Password@123");
+			map.put("productname", "ADIDAS ORIGINAL");
+			
+		HashMap<String, String> map1 =new HashMap<String,String>();	
+			map1.put("username", "Password@123");
+			map1.put("password", "Pramitkoley@mail.com");
+			map1.put("productname", "IPHONE 13 PRO");
+			return new Object[][] {{map},{map1}};
 
 	}
+	
+	
 
 }
